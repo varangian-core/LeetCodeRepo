902b132f1a7f6debf4c0bd8efed2b6915ea67747
varangian-core <mikhaltsevichv@gmail.com>
2023-12-26 00:13:31 -0800

    Refactor HttpClient usage and add content saving feature
    The HttpClient in LeetCodeSnippet has been changed to static and the method GetProblemDetails has been made static as well. Also, the 'buffers' directory is no longer included in the csproj file. A new feature to save problem content retrieved from LeetCode to a buffer directory has been implemented in the Main function of the Program.cs file. The changes provide a streamlined approach for HttpClient usage and enhanced functionality for content retrieval.

--- LeetCode.cs
+++ LeetCode.cs
@@ -7,19 +7,17 @@ using HtmlAgilityPack;
 namespace LeetCodeRepo;
 public class LeetCodeSnippet
 {
-    private HttpClient _client;
+    private static HttpClient _client;
 
     public LeetCodeSnippet()
     {
         _client = new HttpClient();
     }
     
-    public async Task<string> GetProblemDetails(string problemUrl)
+    public static async Task<string> GetProblemDetails(string problemUrl)
     {
         var response = await _client.GetAsync(problemUrl);
         var pageContent= await response.Content.ReadAsStringAsync();
-        
-        //ExtractProblemContent(pageContent);
         return pageContent;
     }
     
--- Program.cs
+++ Program.cs
@@ -1,23 +1,44 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LeetCodeRepo
 {
     class Program
     {
-        public async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var leetCodeSnippet = new LeetCodeSnippet();
             var problemUrl = "https://leetcode.com/problems/two-sum/description/";
-            var problemContent = await LeetCodeSnippet.GetProblemDetails(problemUrl); //let's think if we want this to be static
+            var problemContent = await LeetCodeSnippet.GetProblemDetails(problemUrl); 
             
             var parser = new HtmlContentParser();
             var innerContent = parser.ParseInnerContent(problemContent);
 
 
+            var uri = new Uri(problemUrl);
+            var segments = uri.Segments;
+            var problemName = segments.Length >= 3 ? segments[2].TrimEnd() : "unknown";
+            
+           var fileName = $"{problemName}.html"; 
+            
+
             Console.WriteLine("innerContent"  + problemContent.ToString());
+            
+            var relativePath = @"../../buffers";
+            var filePath= Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), relativePath));
+            var buffersDir = Path.Combine(Directory.GetCurrentDirectory(), "buffers");
+            if (!Directory.Exists(buffersDir))
+            {
+                Directory.CreateDirectory(buffersDir);
+            }
 
+            using (var writer = new StreamWriter(filePath, false))
+            {
+                await writer.WriteAsync(innerContent);
+            } 
 
+            Console.WriteLine($"Content saved to {filePath}");
         }
     }
 }
